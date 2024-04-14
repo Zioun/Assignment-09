5cee45aa01603389e5dfddcfd43fa047ae9e8d40
Zioun <jionkhan0@gmail.com>
2024-04-15 00:22:50 +0600

    issue solve

--- src/Components/Layouts/Root/Root.jsx
+++ src/Components/Layouts/Root/Root.jsx
@@ -5,7 +5,7 @@ import Footer from '../../Pages/Footer/Footer';
 
 const Root = () => {
     return (
-        <div className='font-roboto flex flex-col h-screen'>
+        <div className='font-roboto flex flex-col h-screen overflow-x-hidden'>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
